5d889cee4edfd55cd8398a4cb88bd28ca9038b27
matheuscrebola-faj-bio <matheus.cavalcanti@fajbio.com.br>
2025-11-24 08:40:37 -0300

    first commit

---
+++ backend/danteia/src/main/java/br/com/fajbio/danteia/DanteiaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fajbio.danteia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DanteiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DanteiaApplication.class, args);
+	}
+
+}
---
+++ backend/danteia/src/test/java/br/com/fajbio/danteia/DanteiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fajbio.danteia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DanteiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
