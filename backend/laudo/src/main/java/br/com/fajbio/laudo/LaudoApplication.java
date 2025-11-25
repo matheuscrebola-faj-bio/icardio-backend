322aaa1a89182bfbe43b2e0b95ee816e9dedc01a
matheuscrebola-faj-bio <matheus.cavalcanti@fajbio.com.br>
2025-11-25 10:11:35 -0300

    feat: adição do microserviços de laudos

---
+++ backend/laudo/src/main/java/br/com/fajbio/laudo/LaudoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fajbio.laudo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaudoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaudoApplication.class, args);
+	}
+
+}
