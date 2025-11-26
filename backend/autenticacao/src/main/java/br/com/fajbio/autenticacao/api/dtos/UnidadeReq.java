d3d805dcb110d1a1493c05983f5d165c8d9ae2d1
matheuscrebola-faj-bio <matheus.cavalcanti@fajbio.com.br>
2025-11-26 10:14:13 -0300

    feat: criação dos métodos de serviço + dto

--- backend/autenticacao/src/main/java/br/com/fajbio/autenticacao/api/dtos/UnidadeReq.java
+++ backend/autenticacao/src/main/java/br/com/fajbio/autenticacao/api/dtos/UnidadeReq.java
@@ -1,4 +1,13 @@
 package br.com.fajbio.autenticacao.api.dtos;
 
-public record UnidadeReq() {
+public record UnidadeReq(
+        String nome,
+        String apelido,
+        String cidade,
+        String estado,
+        String endereco,
+        String contato,
+        String email,
+        String telefone
+) {
 }
--- backend/autenticacao/src/main/java/br/com/fajbio/autenticacao/api/dtos/UsuarioReq.java
+++ backend/autenticacao/src/main/java/br/com/fajbio/autenticacao/api/dtos/UsuarioReq.java
@@ -1,4 +1,16 @@
 package br.com.fajbio.autenticacao.api.dtos;
 
-public record UsuarioReq() {
+import br.com.fajbio.autenticacao.domain.enums.EPerfil;
+
+public record UsuarioReq(
+        String nome,
+        String sobrenome,
+        String login,
+        String senha,
+        String documento,
+        String email,
+        String telefone,
+        String assinatura,
+        EPerfil perfil
+) {
 }
