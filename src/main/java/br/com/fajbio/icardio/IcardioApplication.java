5ced41c78f07c9c56287e77e2cbb642f0efbdabb
matheuscrebola-faj-bio <matheus.cavalcanti@fajbio.com.br>
2025-12-05 08:55:47 -0300

    feat: first commit

---
+++ src/main/java/br/com/fajbio/icardio/IcardioApplication.java
@@ -0,0 +1,13 @@
+package br.com.fajbio.icardio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcardioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcardioApplication.class, args);
+	}
+
+}
